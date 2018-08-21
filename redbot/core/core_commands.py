cfe9a436de3df3675379e34932aa99c41b32e401
Caleb Johnson <me@calebj.io>
2018-08-21 00:41:46 -0500

    [backup] split backup into 8MB chunks if needed

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -9,6 +9,7 @@ import sys
 import tarfile
 import traceback
 from collections import namedtuple
+from math import ceil
 from pathlib import Path
 from random import SystemRandom
 from string import ascii_letters, digits
@@ -1089,26 +1090,33 @@ class Core(CoreLogic):
             m = Mongo("Core", **basic_config["STORAGE_DETAILS"])
             db = m.db
             collection_names = await db.collection_names(include_system_collections=False)
+
             for c_name in collection_names:
                 if c_name == "Core":
                     c_data_path = data_dir / basic_config["CORE_PATH_APPEND"]
                 else:
                     c_data_path = data_dir / basic_config["COG_PATH_APPEND"]
+
                 output = {}
                 docs = await db[c_name].find().to_list(None)
+
                 for item in docs:
                     item_id = str(item.pop("_id"))
                     output[item_id] = item
+
                 target = JSON(c_name, data_path_override=c_data_path)
                 await target.jsonIO._threadsafe_save_json(output)
+
         backup_filename = "redv3-{}-{}.tar.gz".format(
             instance_name, ctx.message.created_at.strftime("%Y-%m-%d %H-%M-%S")
         )
+
         if data_dir.exists():
             if not backup_path:
                 backup_pth = data_dir.home()
             else:
                 backup_pth = Path(backup_path)
+
             backup_file = backup_pth / backup_filename
 
             to_backup = []
@@ -1120,29 +1128,63 @@ class Core(CoreLogic):
                 os.path.join("RepoManager", "repos"),
             ]
             downloader_cog = ctx.bot.get_cog("Downloader")
+
             if downloader_cog and hasattr(downloader_cog, "_repo_manager"):
                 repo_output = []
                 repo_mgr = downloader_cog._repo_manager
+
                 for repo in repo_mgr._repos.values():
                     repo_output.append({"url": repo.url, "name": repo.name, "branch": repo.branch})
+
                 repo_filename = data_dir / "cogs" / "RepoManager" / "repos.json"
+
                 with open(str(repo_filename), "w") as f:
                     f.write(json.dumps(repo_output, indent=4))
+
             instance_data = {instance_name: basic_config}
             instance_file = data_dir / "instance.json"
+
             with open(str(instance_file), "w") as instance_out:
                 instance_out.write(json.dumps(instance_data, indent=4))
+
             for f in data_dir.glob("**/*"):
                 if not any(ex in str(f) for ex in exclusions):
                     to_backup.append(f)
+
             with tarfile.open(str(backup_file), "w:gz") as tar:
                 for f in to_backup:
                     tar.add(str(f), recursive=False)
-            print(str(backup_file))
+
+            print("Backup complete. Destination:", backup_file)
+
             await ctx.send(
-                _("A backup has been made of this instance. It is at {}.").format((backup_file))
+                _("A backup has been made of this instance. It is at {}.").format(backup_file)
+                + "\n"
+                + _("Would you like to receive a copy via DM? (y/n)")
             )
-            await ctx.send(_("Would you like to receive a copy via DM? (y/n)"))
+
+            filesize_limit = 8 * 1024 * 1024
+            buf_size = 16 * 1024
+            num_parts = ceil(backup_file.stat().st_size / filesize_limit)
+
+            if num_parts > 1:
+                bytes_copied = 0
+                split_backup_files = []
+
+                with backup_file.open("rb") as src:
+                    for i in range(num_parts):
+                        stop_copying_at = (i + 1) * filesize_limit
+                        part_file = Path(backup_pth / "{}.part{}".format(backup_filename, i))
+                        split_backup_files.append(part_file)
+
+                        with part_file.open("wb") as dest:
+                            while bytes_copied <= stop_copying_at:
+                                buf = src.read(buf_size)
+                                bytes_written = dest.write(buf)
+                                bytes_copied += bytes_written
+
+                                if not bytes_written:
+                                    break
 
             def same_author_check(m):
                 return m.author == ctx.author and m.channel == ctx.channel
@@ -1153,9 +1195,16 @@ class Core(CoreLogic):
                 await ctx.send(_("Ok then."))
             else:
                 if msg.content.lower().strip() == "y":
-                    await ctx.author.send(
-                        _("Here's a copy of the backup"), file=discord.File(str(backup_file))
-                    )
+                    if num_parts > 1:
+                        await ctx.author.send(
+                            _("The backup has been split into {} parts:").format(num_parts)
+                        )
+                        for part_file in split_backup_files:
+                            await ctx.author.send(file=discord.File(str(part_file)))
+                    else:
+                        await ctx.author.send(
+                            _("Here's a copy of the backup"), file=discord.File(str(backup_file))
+                        )
         else:
             await ctx.send(_("That directory doesn't seem to exist..."))
 
